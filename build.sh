f49c8c5ab12a542feb4b5e370cfe690fe2eb6f3d
Kanat Bekt <bekt17@gmail.com>
2017-01-09 21:34:35 -0600

    Sync build.sh (#fc355d6)

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
-apk add --update go git mercurial build-base
+apk add --update go git mercurial build-base ca-certificates
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
 export GOPATH=/go
 go get
-go build -ldflags "-X main.Version $1" -o /bin/logspout
+go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go
 rm -rf /var/cache/apk/*
